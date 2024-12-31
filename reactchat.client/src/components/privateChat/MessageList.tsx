f6281ff6c404247ccdaef4ccd2f5d6121343e419
azhirak <azhirak@AZHIRAK-PC>
2024-12-31 09:23:30 +0330

    تست کامیت

--- reactchat.client/src/components/privateChat/MessageList.tsx
+++ reactchat.client/src/components/privateChat/MessageList.tsx
@@ -14,7 +14,7 @@ interface MessageListProps {
 
 const MessageList: React.FC<MessageListProps> = ({ messages }) => {
   return (
-    <div className="private-message-list">
+    <div className="private-message-list test2222">
       {messages.map((message, index) => (
         <MessageItem key={index} message={message} />
       ))}
